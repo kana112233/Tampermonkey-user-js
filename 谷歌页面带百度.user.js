55722bd546fd3462024cba629a8196a3940374f8
kana112233 <12538530+kana112233@users.noreply.github.com>
2019-03-03 16:13:52 +0800

    Update 谷歌页面带百度.user.js

--- 谷歌页面带百度.user.js
+++ 谷歌页面带百度.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         谷歌页面带百度
-// @namespace    
-// @version      1.0
-// @description  百度搜索加上谷歌按钮
+// @namespace    https://github.com/kana112233/Tampermonkey-user-js
+// @version      1.0.0
+// @description  谷歌页面带百度
+// @description:zh-cn 谷歌页面带百度
 // @author       makey
 // @grant        GM_openInTab
 // @include      https://www.google.com/*
@@ -12,18 +13,18 @@
 (function() {
     'use strict';
 
-   function getBaiduUrl(searchText) {
+   function getBaiduOnceUrl(searchText) {
        return 'https://www.baidu.com/s?wd=' + searchText;
    }
-   function BaiduIt() {
+   function BaiduOnce() {
        var searchText = document.querySelector('.gLFyf').value;
-       GM_openInTab( getBaiduUrl(searchText), false);
+       GM_openInTab( getBaiduOnceUrl(searchText), false);
    }
    $('#gbqfbb').after('<input type="button" id="baiduId" value="百度一下" class="btn self-btn bg s_btn" style="background-color:#f2f2f2;border:1px solid #f2f2f2;border-radius: 4px;width:142.5px;height:36px" />');
    $('#hdtb-msb').after('<input type="button" id="baiduId" value="百度一下" class="btn self-btn bg s_btn" style="background-color:#f2f2f2;border:1px solid #f2f2f2;border-radius: 4px;width:142.5px;height:36px" />');
 
    $("#baiduId").click(function() {
-        BaiduIt();
+        BaiduOnce();
    });
 
 })();
