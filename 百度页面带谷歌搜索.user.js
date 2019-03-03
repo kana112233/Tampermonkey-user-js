48d539f2e518ee868e0477720afb986f94a3c5fe
kana112233 <12538530+kana112233@users.noreply.github.com>
2019-03-03 15:50:44 +0800

    百度页面带谷歌搜索

---
+++ 百度页面带谷歌搜索.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         百度页面带谷歌搜索
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  百度搜索加上谷歌按钮
+// @author       makey
+// @grant        GM_openInTab
+// @include      https://www.baidu.com/*
+// @require      https://cdn.staticfile.org/jquery/1.12.4/jquery.min.js
+// ==/UserScript==
+(function() {
+   'use strict';
+
+   function getGoogleUrl(searchText) {
+       return 'https://www.google.com/search?q=' + searchText;
+   }
+   function googleIt() {
+       var searchText = document.querySelector('#kw').value;
+       GM_openInTab( getGoogleUrl(searchText), false);
+   }
+    $('#su').after('<input type="button" id="google" value="Google一下" class="btn self-btn bg s_btn" style="background-color:grey;" />');
+    $("#google").click(function() {
+        googleIt();
+    });
+
+})();
